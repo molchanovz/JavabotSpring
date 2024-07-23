00dc01c297dfa797e00d7e0c821a21bd28f7d6eb
molchanovz <molchanov.top@gmail.com>
2024-07-23 21:54:19 +0300

    Commit

--- src/main/java/com/example/javabotspring/Logic/Tests/WildberriesTests/TestLastOrders.java
+++ src/main/java/com/example/javabotspring/Logic/Tests/WildberriesTests/TestLastOrders.java
@@ -1,5 +1,6 @@
 package com.example.javabotspring.Logic.Tests.WildberriesTests;
 
+import com.example.javabotspring.Bot.Protection;
 import com.example.javabotspring.Logic.WildberriesHandler.WildberriesApiToMessage;
 import org.json.JSONException;
 
@@ -7,12 +8,7 @@ import java.io.IOException;
 
 public class TestLastOrders {
     public static void main(String[] args) throws IOException, JSONException, InterruptedException {
-        String key = "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjQwNTA2djEiLCJ0eXAiOiJKV1QifQ" +
-                ".eyJlbnQiOjEsImV4cCI6MTczNDAzNDI5NSwiaWQiOiI4NTFjNmU0Yy1hMTI2LTRiOTg" +
-                "tYWM2ZC0yNWU0YmNjNGU4YTUiLCJpaWQiOjQ2MTk4NjUsIm9pZCI6Mjc1NjYsInMiOjQw" +
-                "OTQsInNpZCI6IjYzYTk2NDNlLWM3NTYtNTQ0Yy1iZTkzLTRhM2ExMzI2Njg5MyIsInQiO" +
-                "mZhbHNlLCJ1aWQiOjQ2MTk4NjV9.Kg9wjfJiN19xBXDr90IdEqC3Vtpvrdxboyzxr4JS9" +
-                "KkMl-goanoW_cEWYXo21zrH3ZBN0Q4Pdsm6CaHacyjhsg";
+        String key = Protection.wbKey;
         WildberriesApiToMessage.getLastDayOrders(key);
     }
 
--- src/main/java/com/example/javabotspring/Logic/WildberriesHandler/WildberriesApi.java
+++ src/main/java/com/example/javabotspring/Logic/WildberriesHandler/WildberriesApi.java
@@ -192,7 +192,7 @@ public class WildberriesApi {
             request.setURI(uri);*/
 
             // добавляем заголовки запроса
-            request.setHeader("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6ImNjMzU2MGI4LTk5NjctNDRkOC04OTU2LTI4YjRkYjE3MGZlYSJ9.EpNeXljvhZCh-wMeLgfJ58-xVP7fBF7LgE_AMf_0gRs");
+            request.setHeader("Authorization", "");
             //System.out.println(Arrays.stream(request.getAllHeaders()).toList().toString());
             CloseableHttpResponse response = httpClient.execute(request);
 
@@ -286,12 +286,7 @@ public class WildberriesApi {
             request.setEntity(params);
 
             // добавляем заголовки запроса
-            request.setHeader("Authorization", "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjMxMDI1" +
-                    "djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTcxODE0MTE0NCwiaWQiOiJmYWQwNTFjMC1m" +
-                    "MTc1LTRjMzktOWQ4Yy0zNzc2NDgzMGY5OGEiLCJpaWQiOjQ2MTk4NjUsIm9pZCI6Mjc1NjYsInMiOjQ" +
-                    "0Niwic2FuZGJveCI6ZmFsc2UsInNpZCI6IjYzYTk2NDNlLWM3NTYtNTQ0Yy1iZTkzLTRhM2ExMzI2Nj" +
-                    "g5MyIsInVpZCI6NDYxOTg2NX0.ncQDNGh1vg-10tfwfDP7CoDsEa-XDfkIIqaGNSgRPW32rpl92UDWX" +
-                    "0dgN_SEM1fuZkD6Ce3ZszJ0sQWYv2OzsA");
+            request.setHeader("Authorization", "");
             //System.out.println(Arrays.stream(request.getAllHeaders()).toList().toString());
             CloseableHttpResponse response = httpClient.execute(request);
 
@@ -331,7 +326,7 @@ public class WildberriesApi {
 
 
             // добавляем заголовки запроса
-            request.setHeader("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6ImNjMzU2MGI4LTk5NjctNDRkOC04OTU2LTI4YjRkYjE3MGZlYSJ9.EpNeXljvhZCh-wMeLgfJ58-xVP7fBF7LgE_AMf_0gRs");
+            request.setHeader("Authorization", "");
             //System.out.println(Arrays.stream(request.getAllHeaders()).toList().toString());
             CloseableHttpResponse response = httpClient.execute(request);
 
@@ -371,12 +366,7 @@ public class WildberriesApi {
             request.setEntity(params);
 
             // добавляем заголовки запроса
-            request.setHeader("Authorization", "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjMxMDI1" +
-                    "djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTcxODE0MTE0NCwiaWQiOiJmYWQwNTFjMC1m" +
-                    "MTc1LTRjMzktOWQ4Yy0zNzc2NDgzMGY5OGEiLCJpaWQiOjQ2MTk4NjUsIm9pZCI6Mjc1NjYsInMiOjQ" +
-                    "0Niwic2FuZGJveCI6ZmFsc2UsInNpZCI6IjYzYTk2NDNlLWM3NTYtNTQ0Yy1iZTkzLTRhM2ExMzI2Nj" +
-                    "g5MyIsInVpZCI6NDYxOTg2NX0.ncQDNGh1vg-10tfwfDP7CoDsEa-XDfkIIqaGNSgRPW32rpl92UDWX" +
-                    "0dgN_SEM1fuZkD6Ce3ZszJ0sQWYv2OzsA");
+            request.setHeader("Authorization", "");
             //System.out.println(Arrays.stream(request.getAllHeaders()).toList().toString());
             CloseableHttpResponse response = httpClient.execute(request);
 
